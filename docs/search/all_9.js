e506848493c8539d4531340e93c17b9d0e1dc748
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-10-22 19:30:18 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@f0b4108cffbd0390be2c2d040e36044868f5ec20 🚀

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['validate_5fcsv_0',['validate_csv',['../csv__data__frame_8hpp.html#aad2891e976554ba3f649575971d80ddd',1,'takane::data_frame']]],
+  ['validate_5fcsv_0',['validate_csv',['../csv__data__frame_8hpp.html#a7cf196659784cf5b89530901bcadea65',1,'takane::data_frame::validate_csv(Reader &amp;reader, size_t num_rows, bool has_row_names, const std::vector&lt; ColumnDetails &gt; &amp;columns, const comservatory::ReadOptions &amp;options, int df_version=2)'],['../csv__data__frame_8hpp.html#a8796353be30b1b0da2e073344ba4ac7a',1,'takane::data_frame::validate_csv(const char *path, size_t num_rows, bool has_row_names, const std::vector&lt; ColumnDetails &gt; &amp;columns, const comservatory::ReadOptions &amp;options, int df_version=2)']]],
   ['validate_5fhdf5_1',['validate_hdf5',['../hdf5__data__frame_8hpp.html#a183f3468b8e3e2a0a7ada6355052b819',1,'takane::data_frame::validate_hdf5(const H5::Group &amp;handle, hsize_t num_rows, bool has_row_names, const std::vector&lt; ColumnDetails &gt; &amp;columns, int df_version=2, int hdf5_version=2)'],['../hdf5__data__frame_8hpp.html#a89e856974d5c3574a68e2d2ca6698f04',1,'takane::data_frame::validate_hdf5(const std::string &amp;path, const std::string &amp;name, hsize_t num_rows, bool has_row_names, const std::vector&lt; ColumnDetails &gt; &amp;columns, int df_version=2, int hdf5_version=2)']]]
 ];
