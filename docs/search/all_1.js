2f719f9dda134708933f866380ffdc4c4e8268d9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-10-29 04:12:26 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@cd6d95d315bb0f38be8ed804c6b1f58f52f84f98 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['buffer_5fsize_0',['buffer_size',['../structtakane_1_1hdf5__data__frame_1_1Options.html#a9d61df87f9f5a592250fc5e96e7db08b',1,'takane::hdf5_data_frame::Options']]]
+  ['buffer_5fsize_0',['buffer_size',['../structtakane_1_1hdf5__data__frame_1_1Options.html#a9d61df87f9f5a592250fc5e96e7db08b',1,'takane::hdf5_data_frame::Options::buffer_size()'],['../structtakane_1_1hdf5__sparse__matrix_1_1Options.html#a4790f09841ea62578a50ec8f7a12599e',1,'takane::hdf5_sparse_matrix::Options::buffer_size()']]]
 ];
--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['hdf5_5fdata_5fframe_2ehpp_0',['hdf5_data_frame.hpp',['../hdf5__data__frame_8hpp.html',1,'']]],
-  ['hdf5_5fversion_1',['hdf5_version',['../structtakane_1_1hdf5__data__frame_1_1Options.html#ac263323b3585fa4c9b9052590ee90088',1,'takane::hdf5_data_frame::Options']]]
+  ['hdf5_5fsparse_5fmatrix_2ehpp_1',['hdf5_sparse_matrix.hpp',['../hdf5__sparse__matrix_8hpp.html',1,'']]],
+  ['hdf5_5fversion_2',['hdf5_version',['../structtakane_1_1hdf5__data__frame_1_1Options.html#ac263323b3585fa4c9b9052590ee90088',1,'takane::hdf5_data_frame::Options']]]
 ];
