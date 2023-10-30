5eefbf1c091d08c650a643151740c701ccf2b1a9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-10-30 01:57:48 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@925d281438618896c354bc28d958c2022f53bd69 🚀

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['genomic_5franges_2ehpp_0',['genomic_ranges.hpp',['../genomic__ranges_8hpp.html',1,'']]],
   ['get_1',['get',['../structtakane_1_1WrappedOption.html#a6c4c407bbb10f7775dcc22f92bc838dc',1,'takane::WrappedOption']]],
-  ['group_2',['group',['../structtakane_1_1hdf5__sparse__matrix_1_1Parameters.html#a7160e35d19a88f566f162209c3e3b664',1,'takane::hdf5_sparse_matrix::Parameters']]]
+  ['group_2',['group',['../structtakane_1_1hdf5__data__frame_1_1Parameters.html#a0d50ca7b49cdcf027b4c8abb45e39b21',1,'takane::hdf5_data_frame::Parameters::group()'],['../structtakane_1_1hdf5__sparse__matrix_1_1Parameters.html#a7160e35d19a88f566f162209c3e3b664',1,'takane::hdf5_sparse_matrix::Parameters::group()']]]
 ];
--- docs/search/variables_4.js
+++ docs/search/variables_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['group_0',['group',['../structtakane_1_1hdf5__sparse__matrix_1_1Parameters.html#a7160e35d19a88f566f162209c3e3b664',1,'takane::hdf5_sparse_matrix::Parameters']]]
+  ['group_0',['group',['../structtakane_1_1hdf5__data__frame_1_1Parameters.html#a0d50ca7b49cdcf027b4c8abb45e39b21',1,'takane::hdf5_data_frame::Parameters::group()'],['../structtakane_1_1hdf5__sparse__matrix_1_1Parameters.html#a7160e35d19a88f566f162209c3e3b664',1,'takane::hdf5_sparse_matrix::Parameters::group()']]]
 ];
