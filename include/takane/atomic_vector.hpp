22b51b36521020cf1d2d0979ef365dfc19ba1ee3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-17 07:21:58 -0800

    Begin transitioning to the simplified specifications.

--- include/takane/atomic_vector.hpp
+++ include/takane/atomic_vector.hpp
@@ -21,157 +21,115 @@ namespace takane {
 namespace atomic_vector {
 
 /**
- * Type of the atomic vector.
- *
- * - `INTEGER`: a number that can be represented by a 32-bit signed integer.
- * - `NUMBER`: a number that can be represented by a double-precision float.
- * - `STRING`: a string.
- * - `BOOLEAN`: a boolean.
- */
-enum class Type {
-    INTEGER,
-    NUMBER,
-    STRING,
-    BOOLEAN
-};
-
-/**
  * @brief Parameters for validating the atomic vector file.
  */
-struct Parameters {
-    /**
-     * Length of the atomic vector.
-     */
-    size_t length = 0;
-
-    /** 
-     * Type of the atomic vector.
-     */
-    Type type = Type::INTEGER;
-
-    /**
-     * Whether the vector is named.
-     */
-    bool has_names = false;
-
-    /**
-     * Whether to load and parse the file in parallel, see `comservatory::ReadOptions` for details.
-     */
-    bool parallel = false;
-
-    /**
-     * Version of the `atomic_vector` format.
-     */
-    int version = 1;
-};
+struct Parameters {};
 
 /**
- * @cond
- */
-template<class ParseCommand>
-CsvContents validate_base(ParseCommand parse, const Parameters& params, CsvFieldCreator* creator) {
-    DummyCsvFieldCreator default_creator;
-    if (creator == NULL) {
-        creator = &default_creator;
-    }
-
-    comservatory::Contents contents;
-    CsvContents output;
-    if (params.has_names) {
-        auto ptr = creator->string();
-        output.fields.emplace_back(ptr);
-        contents.fields.emplace_back(new CsvNameField(false, ptr));
-    }
-
-    switch(params.type) {
-        case Type::INTEGER:
-            {
-                auto ptr = creator->integer();
-                output.fields.emplace_back(ptr);
-                contents.fields.emplace_back(new CsvIntegerField(static_cast<int>(params.has_names), ptr));
-            }
-            break;
-        case Type::NUMBER:
-            {
-                auto ptr = creator->number();
-                output.fields.emplace_back(nullptr);
-                contents.fields.emplace_back(ptr);
-            }
-            break;
-        case Type::STRING:
-            {
-                auto ptr = creator->string();
-                output.fields.emplace_back(nullptr);
-                contents.fields.emplace_back(ptr);
-            }
-            break;
-        case Type::BOOLEAN:
-            {
-                auto ptr = creator->boolean();
-                output.fields.emplace_back(nullptr);
-                contents.fields.emplace_back(ptr);
-            }
-            break;
-    }
-
-    comservatory::ReadOptions opt;
-    opt.parallel = params.parallel;
-    parse(contents, opt);
-    if (contents.num_records() != params.length) {
-        throw std::runtime_error("number of records in the CSV file does not match the expected length");
-    }
-
-    if (contents.names.back() != "values") {
-        throw std::runtime_error("column containing vector contents should be named 'values'");
-    }
-
-    output.reconstitute(contents.fields);
-    return output;
-}
-/**
- * @endcond
- */
-
-/**
- * Checks if a CSV is correctly formatted for the `atomic_vector` format.
- * An error is raised if the file does not meet the specifications.
- *
- * @tparam Reader A **byteme** reader class.
- *
- * @param reader A stream of bytes from the CSV file.
+ * @param path Path to the directory containing the atomic vector.
  * @param params Validation parameters.
- * @param creator Factory to create objects for holding the contents of each CSV field.
- * Defaults to a pointer to a `DummyFieldCreator` instance.
- *
- * @return Contents of the loaded CSV.
- * Whether the `fields` member actually contains the CSV data depends on `creator`.
- * If `params.has_names = true`, an additional column containing names is present at the start.
  */
-template<class Reader>
-CsvContents validate(Reader& reader, const Parameters& params, CsvFieldCreator* creator = NULL) {
-    return validate_base(
-        [&](comservatory::Contents& contents, const comservatory::ReadOptions& opts) -> void { comservatory::read(reader, contents, opts); },
-        params,
-        creator
-    );
-}
+inline void validate(const string& path, const Parameters& params) try {
+    H5::H5File handle(path, H5F_ACC_RDONLY);
 
-/**
- * Overload of `atomic_vector::validate()` that takes a file path.
- *
- * @param path Path to the CSV file.
- * @param params Validation parameters.
- * @param creator Factory to create objects for holding the contents of each CSV field.
- * Defaults to a pointer to a `DummyFieldCreator` instance.
- *
- * @return Contents of the loaded CSV.
- */
-inline CsvContents validate(const char* path, const Parameters& params, CsvFieldCreator* creator = NULL) {
-    return validate_base(
-        [&](comservatory::Contents& contents, const comservatory::ReadOptions& opts) -> void { comservatory::read_file(path, contents, opts); },
-        params,
-        creator
-    );
+    if (handle.exists("atomic_vector") || handle.childObjType("atomic_vector") != H5O_TYPE_GROUP) {
+        throw std::runtime_error("expected an 'atomic_vector' group");
+    }
+    auto ghandle = handle.openGroup("atomic_vector");
+
+    ritsuko::Version version;
+    auto vstring = ritsuko::hdf5::load_scalar_string_attribute(ghandle, "version");
+    auto version = ritsuko::parse_version_string(vstring.c_str(), vstring.size(), /* skip_patch = */ true);
+    if (version.major != 1) {
+        throw std::runtime_error("unsupported version string '" + vstring + "'");
+    }
+
+    auto dhandle = ritsuko::hdf5::get_dataset(ghandle, "values");
+    auto vlen = ritsuko::hdf5::get_1d_length(dhandle.getSpace(), false);
+    auto type = ritsuko::hdf5::load_scalar_string_attribute(ghandle, "type");
+
+    const std::string missing_attr_name = "missing-value-placeholder";
+    bool has_missing = dhandle.attrExists(missing_attr);
+    H5::Attribute missing_attr; 
+    if (has_missing) {
+        missing_attr = ritsuko::hdf5::get_missing_placeholder_attribute(dhandle, missing_attr);
+    }
+
+    if (type == "integer") {
+        if (ritsuko::hdf5::exceeds_integer_limit(dhandle, 32, true)) {
+            throw std::runtime_error("expected a datatype for 'values' that fits in a 32-bit signed integer");
+        }
+    } else if (type == "boolean") {
+        if (ritsuko::hdf5::exceeds_integer_limit(dhandle, 32, true)) {
+            throw std::runtime_error("expected a datatype for 'values' that fits in a 32-bit signed integer");
+        }
+    } else if (type == "number") {
+        if (ritsuko::hdf5::exceeds_float_limit(dhandle, 64)) {
+            throw std::runtime_error("expected a datatype for 'values' that fits in a 64-bit float");
+        }
+    } else if (type == "string") {
+        if (dhandle.getTypeClass() != H5T_STRING) {
+            throw std::runtime_error("expected a string datatype for 'values'");
+        }
+
+        std::string missing_value;
+        if (has_missing) {
+            missing_value = ritsuko::hdf5::load_scalar_string_attribute(missing_attr);
+        }
+
+        if (dhandle.attrExists("format")) {
+            auto format = ritsuko::hdf5::load_scalar_string_attribute(xhandle, "format");
+            if (format == "date") {
+                ritsuko::hdf5::load_1d_string_dataset(
+                    xhandle, 
+                    vlen,
+                    params.buffer_size,
+                    [&](size_t, const char* p, size_t l) {
+                        std::string x(p, p + l);
+                        if (has_missing && missing_value == x) {
+                            return;
+                        }
+                        if (!ritsuko::is_date(p, l)) {
+                            throw std::runtime_error("expected a date-formatted string (got '" + x + "')");
+                        }
+                    }
+                });
+
+            } else if (format == "date-time") {
+                ritsuko::hdf5::load_1d_string_dataset(
+                    xhandle, 
+                    vlen,
+                    params.buffer_size,
+                    [&](size_t, const char* p, size_t l) {
+                        std::string x(p, p + l);
+                        if (has_missing && missing_value == x) {
+                            return;
+                        }
+                        if (!ritsuko::is_rfc3339(p, l)) {
+                            throw std::runtime_error("expected a date/time-formatted string (got '" + x + "')");
+                        }
+                    }
+                );
+
+            } else if (format != "none") {
+                throw std::runtime_error("unsupported 'format' attribute (got '" + format + "')");
+            }
+        }
+    }
+
+    if (ghandle.exists("names")) {
+        auto nhandle = ritsuko::hdf5::get_dataset(ghandle, "names");
+        if (nhandle.getTypeClass() != H5T_STRING) {
+            throw std::runtime_error("'names' should be a string type class");
+        }
+        auto nlen = ritsuko::hdf5::get_1d_length(nhandle.getSpace(), false);
+        if (vlen != nlen) {
+            throw std::runtime_error("'names' and 'values' should have the same length");
+        }
+    }
+
+} catch (std::exception& e) {
+    throw std::runtime_error("failed to validate an 'atomic_vector'; " + std::string(e.what()));
 }
 
 }
