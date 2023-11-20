cca9cc38b2ec2d1df0f030629f008ea115d78ddd
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-20 21:40:11 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@76e0d0ca378be1408775065d1d75e243e45c3c71 🚀

--- docs/search/variables_8.js
+++ docs/search/variables_8.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['num_5flevels_0',['num_levels',['../structtakane_1_1factor_1_1Parameters.html#a9bee85aed689ee99609f3303654f7e5f',1,'takane::factor::Parameters']]],
   ['num_5franges_1',['num_ranges',['../structtakane_1_1genomic__ranges_1_1Parameters.html#af678af300b3d0ad11ceeab0662cb3f10',1,'takane::genomic_ranges::Parameters']]],
-  ['num_5frows_2',['num_rows',['../structtakane_1_1csv__data__frame_1_1Parameters.html#a5470182fe9fbbd21a97cf3964f8935c4',1,'takane::csv_data_frame::Parameters']]],
-  ['num_5fsequences_3',['num_sequences',['../structtakane_1_1sequence__information_1_1Parameters.html#a758e864f6bcb3be654c5bef19ce555ac',1,'takane::sequence_information::Parameters']]]
+  ['num_5frows_2',['num_rows',['../structtakane_1_1csv__data__frame_1_1Parameters.html#a5470182fe9fbbd21a97cf3964f8935c4',1,'takane::csv_data_frame::Parameters']]]
 ];
