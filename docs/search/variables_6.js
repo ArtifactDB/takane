991de2a39fe277aeb751b8c98fcc7339b7552ae7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-20 01:13:03 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@973d2bd365b7bf76746a2d7d84bcbd50e273f4f9 🚀

--- docs/search/variables_6.js
+++ docs/search/variables_6.js
@@ -4,6 +4,5 @@ var searchData=
   ['has_5fnames_1',['has_names',['../structtakane_1_1compressed__list_1_1Parameters.html#a74fb4711a6dfbdcc6ed5efbde7f91b16',1,'takane::compressed_list::Parameters::has_names()'],['../structtakane_1_1factor_1_1Parameters.html#a562618bd2ff92f55edc1ea70d69a9363',1,'takane::factor::Parameters::has_names()'],['../structtakane_1_1genomic__ranges_1_1Parameters.html#a94016d49ea1853288fbd21fea5cf3b61',1,'takane::genomic_ranges::Parameters::has_names()']]],
   ['has_5frow_5fnames_2',['has_row_names',['../structtakane_1_1csv__data__frame_1_1Parameters.html#a50be7db2e98b55402086fe669e7e05ea',1,'takane::csv_data_frame::Parameters']]],
   ['hdf5_5fbuffer_5fsize_3',['hdf5_buffer_size',['../structtakane_1_1Options.html#a3a9ebb8a77372c852d87f5368b6516c3',1,'takane::Options']]],
-  ['height_5foverride_4',['height_override',['../namespacetakane.html#a5cfaf9a502d948cff4afb2fd861d07a8',1,'takane']]],
-  ['height_5fregistry_5',['height_registry',['../namespacetakane.html#ab68b0822f2450305203a7887c6802ac8',1,'takane']]]
+  ['height_5fregistry_4',['height_registry',['../namespacetakane.html#ab68b0822f2450305203a7887c6802ac8',1,'takane']]]
 ];
