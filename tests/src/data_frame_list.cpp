a9f53183864a247c4a1daa3522fbad758433360e
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-11-22 16:56:59 -0800

    Added validators for all compressed list subclasses. (#17)
    This includes the atomic_vector_list, data_frame_list and genomic_ranges_list.
    We need to apply some small changes to the data_frame and atomic_vector mocking
    functions to more easily generate the concatenated subdirectory.

---
+++ tests/src/data_frame_list.cpp
@@ -0,0 +1,76 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "takane/takane.hpp"
+#include "utils.h"
+#include "data_frame.h"
+#include "simple_list.h"
+
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+struct DataFrameListTest : public::testing::Test {
+    DataFrameListTest() {
+        dir = "TEST_data_frame_list";
+        name = "data_frame_list";
+    }
+
+    std::filesystem::path dir;
+    std::string name;
+
+    H5::H5File initialize() {
+        initialize_directory(dir, name);
+        return H5::H5File(dir / "partitions.h5", H5F_ACC_TRUNC);
+    }
+
+    H5::H5File reopen() {
+        return H5::H5File(dir / "partitions.h5", H5F_ACC_RDWR);
+    }
+
+    void expect_error(const std::string& msg) {
+        EXPECT_ANY_THROW({
+            try {
+                takane::validate(dir);
+            } catch (std::exception& e) {
+                EXPECT_THAT(e.what(), ::testing::HasSubstr(msg));
+                throw;
+            }
+        });
+    }
+};
+
+TEST_F(DataFrameListTest, Basic) {
+    {
+        auto handle = initialize();
+        auto ghandle = handle.createGroup(name);
+        hdf5_utils::attach_attribute(ghandle, "version", "2.0");
+    }
+    expect_error("unsupported version string");
+
+    {
+        auto handle = reopen();
+        auto ghandle = handle.openGroup(name);
+        ghandle.removeAttr("version");
+        hdf5_utils::attach_attribute(ghandle, "version", "1.0");
+        hdf5_utils::spawn_numeric_data<int>(ghandle, "lengths", H5::PredType::NATIVE_UINT32, { 4, 3, 2, 1 });
+        initialize_directory(dir / "concatenated", "foobar");
+    }
+    expect_error("satisfy the 'DATA_FRAME'");
+
+    {
+        initialize_directory(dir / "concatenated", "data_frame");
+    }
+    expect_error("failed to validate the 'concatenated'");
+
+    {
+        data_frame::mock(dir / "concatenated", 7, false, {});
+    }
+    expect_error("sum of 'lengths'");
+
+    {
+        data_frame::mock(dir / "concatenated", 10, false, {});
+    }
+    takane::validate(dir);
+    EXPECT_EQ(takane::height(dir), 4);
+}
---
+++ tests/src/genomic_ranges_list.cpp
@@ -0,0 +1,77 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "takane/takane.hpp"
+#include "utils.h"
+#include "genomic_ranges.h"
+#include "data_frame.h"
+#include "simple_list.h"
+
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+struct GenomicRangesListTest : public::testing::Test {
+    GenomicRangesListTest() {
+        dir = "TEST_genomic_ranges_list";
+        name = "genomic_ranges_list";
+    }
+
+    std::filesystem::path dir;
+    std::string name;
+
+    H5::H5File initialize() {
+        initialize_directory(dir, name);
+        return H5::H5File(dir / "partitions.h5", H5F_ACC_TRUNC);
+    }
+
+    H5::H5File reopen() {
+        return H5::H5File(dir / "partitions.h5", H5F_ACC_RDWR);
+    }
+
+    void expect_error(const std::string& msg) {
+        EXPECT_ANY_THROW({
+            try {
+                takane::validate(dir);
+            } catch (std::exception& e) {
+                EXPECT_THAT(e.what(), ::testing::HasSubstr(msg));
+                throw;
+            }
+        });
+    }
+};
+
+TEST_F(GenomicRangesListTest, Basic) {
+    {
+        auto handle = initialize();
+        auto ghandle = handle.createGroup(name);
+        hdf5_utils::attach_attribute(ghandle, "version", "2.0");
+    }
+    expect_error("unsupported version string");
+
+    {
+        auto handle = reopen();
+        auto ghandle = handle.openGroup(name);
+        ghandle.removeAttr("version");
+        hdf5_utils::attach_attribute(ghandle, "version", "1.0");
+        hdf5_utils::spawn_numeric_data<int>(ghandle, "lengths", H5::PredType::NATIVE_UINT32, { 1, 2, 1, 3 });
+        initialize_directory(dir / "concatenated", "foobar");
+    }
+    expect_error("'genomic_ranges'");
+
+    {
+        initialize_directory(dir / "concatenated", "genomic_ranges");
+    }
+    expect_error("failed to validate the 'concatenated'");
+
+    {
+        genomic_ranges::mock(dir / "concatenated", 8, 5);
+    }
+    expect_error("sum of 'lengths'");
+
+    {
+        genomic_ranges::mock(dir / "concatenated", 7, 3);
+    }
+    takane::validate(dir);
+    EXPECT_EQ(takane::height(dir), 4);
+}
