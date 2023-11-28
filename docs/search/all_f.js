adb4dfefe1d9ad7dfb4c67a9ff2e653580852ec2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-28 22:55:40 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@204b1ca85f916fb034380a8623b7b5b90e2140f0 🚀

--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -6,9 +6,10 @@ var searchData=
   ['data_5fframe_5ffactor_3',['data_frame_factor',['../namespacetakane_1_1data__frame__factor.html',1,'takane']]],
   ['dense_5farray_4',['dense_array',['../namespacetakane_1_1dense__array.html',1,'takane']]],
   ['genomic_5franges_5',['genomic_ranges',['../namespacetakane_1_1genomic__ranges.html',1,'takane']]],
-  ['sequence_5finformation_6',['sequence_information',['../namespacetakane_1_1sequence__information.html',1,'takane']]],
-  ['simple_5flist_7',['simple_list',['../namespacetakane_1_1simple__list.html',1,'takane']]],
-  ['string_5ffactor_8',['string_factor',['../namespacetakane_1_1string__factor.html',1,'takane']]],
-  ['summarized_5fexperiment_9',['summarized_experiment',['../namespacetakane_1_1summarized__experiment.html',1,'takane']]],
-  ['takane_10',['takane',['../namespacetakane.html',1,'']]]
+  ['ranged_5fsummarized_5fexperiment_6',['ranged_summarized_experiment',['../namespacetakane_1_1ranged__summarized__experiment.html',1,'takane']]],
+  ['sequence_5finformation_7',['sequence_information',['../namespacetakane_1_1sequence__information.html',1,'takane']]],
+  ['simple_5flist_8',['simple_list',['../namespacetakane_1_1simple__list.html',1,'takane']]],
+  ['string_5ffactor_9',['string_factor',['../namespacetakane_1_1string__factor.html',1,'takane']]],
+  ['summarized_5fexperiment_10',['summarized_experiment',['../namespacetakane_1_1summarized__experiment.html',1,'takane']]],
+  ['takane_11',['takane',['../namespacetakane.html',1,'']]]
 ];
