ed730b2ea7cdc2794bfb8fc3c58b5b8c43a18363
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-12-12 20:50:49 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@db4b55e253750de7aa136e54b93f0edd5d4289ca 🚀

--- docs/search/functions_5.js
+++ docs/search/functions_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['read_5fobject_5fmetadata_0',['read_object_metadata',['../namespacetakane.html#a4202d62d10c0d3d6ad07085f01736372',1,'takane']]]
+  ['read_5fobject_5fmetadata_0',['read_object_metadata',['../namespacetakane.html#a4202d62d10c0d3d6ad07085f01736372',1,'takane']]],
+  ['reformat_5fobject_5fmetadata_1',['reformat_object_metadata',['../namespacetakane.html#aeb6711d9cf4ba4e408c1890914ad84b2',1,'takane']]]
 ];
