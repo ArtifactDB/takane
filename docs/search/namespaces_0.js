c2c510cac87522646817ed32411227f865bd4e9e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-12-25 19:33:09 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@6fc939de90a96ee93c2336d2ae8142fc39f79cf7 🚀

--- docs/search/namespaces_0.js
+++ docs/search/namespaces_0.js
@@ -6,11 +6,12 @@ var searchData=
   ['data_5fframe_5ffactor_3',['data_frame_factor',['../namespacetakane_1_1data__frame__factor.html',1,'takane']]],
   ['dense_5farray_4',['dense_array',['../namespacetakane_1_1dense__array.html',1,'takane']]],
   ['genomic_5franges_5',['genomic_ranges',['../namespacetakane_1_1genomic__ranges.html',1,'takane']]],
-  ['ranged_5fsummarized_5fexperiment_6',['ranged_summarized_experiment',['../namespacetakane_1_1ranged__summarized__experiment.html',1,'takane']]],
-  ['sequence_5finformation_7',['sequence_information',['../namespacetakane_1_1sequence__information.html',1,'takane']]],
-  ['simple_5flist_8',['simple_list',['../namespacetakane_1_1simple__list.html',1,'takane']]],
-  ['single_5fcell_5fexperiment_9',['single_cell_experiment',['../namespacetakane_1_1single__cell__experiment.html',1,'takane']]],
-  ['string_5ffactor_10',['string_factor',['../namespacetakane_1_1string__factor.html',1,'takane']]],
-  ['summarized_5fexperiment_11',['summarized_experiment',['../namespacetakane_1_1summarized__experiment.html',1,'takane']]],
-  ['takane_12',['takane',['../namespacetakane.html',1,'']]]
+  ['multi_5fsample_5fdataset_6',['multi_sample_dataset',['../namespacetakane_1_1multi__sample__dataset.html',1,'takane']]],
+  ['ranged_5fsummarized_5fexperiment_7',['ranged_summarized_experiment',['../namespacetakane_1_1ranged__summarized__experiment.html',1,'takane']]],
+  ['sequence_5finformation_8',['sequence_information',['../namespacetakane_1_1sequence__information.html',1,'takane']]],
+  ['simple_5flist_9',['simple_list',['../namespacetakane_1_1simple__list.html',1,'takane']]],
+  ['single_5fcell_5fexperiment_10',['single_cell_experiment',['../namespacetakane_1_1single__cell__experiment.html',1,'takane']]],
+  ['string_5ffactor_11',['string_factor',['../namespacetakane_1_1string__factor.html',1,'takane']]],
+  ['summarized_5fexperiment_12',['summarized_experiment',['../namespacetakane_1_1summarized__experiment.html',1,'takane']]],
+  ['takane_13',['takane',['../namespacetakane.html',1,'']]]
 ];
