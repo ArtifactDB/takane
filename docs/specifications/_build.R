c41eb0c053dc923d1eed2fdb83d17029f7a96504
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2024-01-04 12:10:29 -0800

    Add human-readable documentation for all the supported formats. (#29)

---
+++ docs/specifications/_build.R
@@ -0,0 +1,22 @@
+library(knitr)
+
+listings <- list.files(pattern="\\.Rmd$")
+default <- "1.0"
+known.variants <- list()
+
+dest <- "compiled"
+unlink(dest, recursive=TRUE)
+dir.create(dest)
+
+for (n in listings) {
+    versions <- c(default, known.variants[[n]])
+    odir <- file.path(dest, sub("\\.Rmd$", "", n))
+    unlink(odir, recursive=TRUE)
+    dir.create(odir)
+    for (v in versions) {
+        .version <- package_version(v)
+        knitr::knit(n, file.path(odir, paste0(v, ".md")))
+    }
+}
+
+file.copy("_general.md", dest)
