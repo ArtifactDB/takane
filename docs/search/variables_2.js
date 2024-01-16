b5eb1ac569b9ec0d622d0234fca14b4800b036d1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-16 08:03:52 +0000

    Deploying to gh-pages from @ ArtifactDB/takane@cdb23ab8e93cecb8c5c3a41fb3d5e8a567c08f02 🚀

--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['data_5fframe_5ffactor_5fany_5fduplicated_0',['data_frame_factor_any_duplicated',['../structtakane_1_1Options.html#a4b00bcdd60587f12d8686f4d03acf38d',1,'takane::Options']]],
-  ['df_5fversion_1',['df_version',['../structtakane_1_1csv__data__frame_1_1Parameters.html#a88bf5f67b1cb4b87834fd63fb48fe285',1,'takane::csv_data_frame::Parameters']]]
+  ['delayed_5farray_5foptions_1',['delayed_array_options',['../structtakane_1_1Options.html#a04925279d99322a9a41c33f091d400e9',1,'takane::Options']]],
+  ['df_5fversion_2',['df_version',['../structtakane_1_1csv__data__frame_1_1Parameters.html#a88bf5f67b1cb4b87834fd63fb48fe285',1,'takane::csv_data_frame::Parameters']]]
 ];
